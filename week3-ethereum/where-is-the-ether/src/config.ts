bf60a74bbcccbadf51c52f66771de798e3ad6b79
caaatisgood <caaatisgood@gmail.com>
2023-07-23 14:58:56 +0100

    Add comment for pk

--- week3-ethereum/where-is-the-ether/src/config.ts
+++ week3-ethereum/where-is-the-ether/src/config.ts
@@ -1,6 +1,12 @@
 import { ethers } from 'ethers'
 import Ganache from 'ganache'
 
+/**
+ * [IMPORTANT NOTICE]
+ * 
+ * This is a PK from the course website. In reality this should be stored
+ * in a `.env` file and should NEVER be committed to git history.
+ */
 export const PRIVATE_KEY = '0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d'
 const INITIAL_BALANCE = ethers.parseEther('10')
 

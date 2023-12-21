c781382dac502e5449663459403b68247375150a
caaatisgood <caaatisgood@gmail.com>
2023-12-21 16:19:34 +0000

    w4 deploy contract w/ hardhat

---
+++ week4-smart-contract-basics/deploy-contract/contracts/Faucet.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.9;
+
+contract Faucet {
+  
+  function withdraw(uint _amount) public {
+    require(_amount <= 100000000000000000);
+    payable(msg.sender).transfer(_amount);
+  }
+
+  receive() external payable {}
+}
